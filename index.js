b14974c0ee8a3206504a5a026f35876bc3755bec
jon199835-crypto <jon199835@gmail.com>
2025-08-31 01:14:53 +0300

    index.js

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// тестовый корневой маршрут
 app.get("/", (req, res) => {
   res.send("Ozon Proxy Server is running ✅. Используй /product/:sku");
 });
 
-// маршрут для SKU
 app.get("/product/:sku", (req, res) => {
   const sku = req.params.sku;
   res.json({ message: "Работает!", sku: sku });
