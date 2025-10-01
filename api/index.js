aeeeb293d22b39c0f785c039f85bb8e21bc9e0a0
jon199835-crypto <jon199835@gmail.com>
2025-10-01 14:33:34 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
     console.log("📥 Telegram update:", JSON.stringify(update));
 
     // Пересылаем напрямую в Apps Script (googleusercontent URL, без 302 редиректа)
-    const response = await fetch("https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLjk7K5V-z2JAIibklkiQXCMqtQ1nkfR2TpRq0AkcyI7bLsiiTJ7WJdgVyRQFZDvBoNQPkCpDQk3YaDWk5nUYdtFkPOIkMTjYqHP8KtPlbmqp14OgYtCGPo4uC5X2ruDiRJhTfOrE4jIY1YDJPySPAeKAMLPR5lTS96Y0HS8mESVVmztdaJ9l_ZLpGIJVbqhYc7-44SonYbBItpZSmzFwg-QZS0KRwG0D226OzSb5I5kFA4Ndakk4MybwSIlar7ByGPyyZDffoCnlQrv027C_QxP8WCjag&lib=MWU1KQnTxyXH7F9CuBMTqaVnitNJBIhCK", {
+    const response = await fetch("https://script.google.com/macros/s/AKfycby09uPOCTy1XW6mFxjU5i6zOn0y0DUImXNJqnYJ_SGLcdAj4u8np2CN8WPBu819yUyY/exec", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(update)
