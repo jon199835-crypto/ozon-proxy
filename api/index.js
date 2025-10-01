9e4bd0777c4689eef357289e01cbf61aa5a21cef
jon199835-crypto <jon199835@gmail.com>
2025-10-01 14:52:06 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const GAS_EXEC_URL = "https://script.googleusercontent.com/macros/echo?user_content_key=...&lib=..."; 
+  const GAS_EXEC_URL = "https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLifCWLCYfkqmQZfAz_PWr-lBseCHpycvHh6K-NwecC-s6lpr4GuP4reBJNba9gJB3WyBv-FpcaHr--JErbA-NqwG-hL3BIVZ0TYRH8K5FoDzoXZnWuYwHZfzUumgiDbDMpEBtPskPPRLPdyPhDhZUxzfJEHQLy8l-OXxYGNf95xZiIuFVhRM4qC860AIwM2QtUVCAUe5WHRz9ceVL79EBrRXgwpm9UvIiNv7e2XHdlefJDc4OnRPRPjW11UiqkauZaGKpgbYO71IQYv2iciis2yxlBmMA&lib=MWU1KQnTxyXH7F9CuBMTqaVnitNJBIhCK"; 
   // сюда вставь свой свежий googleusercontent-URL
 
   if (req.method !== 'POST') {
